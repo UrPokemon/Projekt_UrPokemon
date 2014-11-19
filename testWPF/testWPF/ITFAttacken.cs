f60ef1bf859cb2f8f47ee87e76bee25a33766003
Fabian <fabian.feichter@students.htlinn.ac.at>
2014-11-19 08:13:17 +0100

    Tagesberichte und WPF

--- testWPF/testWPF/ITFAttacken.cs
+++ testWPF/testWPF/ITFAttacken.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace testWPF
 {
-    interface ITFAttaken
+   public interface ITFAttaken
     {
         void angriff(double staerke, string name, double accuracy);
     }
